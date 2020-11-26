844b53d55ce4cf2e43d45abb9a6a098b3c7cd1fa
Fairuz Zamrody <61002522+fairuzzamrody@users.noreply.github.com>
2020-11-26 19:53:46 +0700

    Add files via upload

---
+++ Class Member.java
@@ -0,0 +1,17 @@
+public class Member {
+    private String nama;
+    private int umur;
+    
+    public class Member(String nama , int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+    public void getUmur(int umur){
+        this.nama = nama;
+    }
+    public void display(){
+        System.out.println("Nama : " + this.name);
+        System.out.println("Umur : " + this.name);
+        
+    }
+}
---
+++ Trainee.java
@@ -0,0 +1,17 @@
+public class Trainee extends Member{
+    private int lamaTraining;
+    
+    public Trainee(String nama , int umur , int lamaTraining){
+        super(nama,umur);
+        this.lamaTraining = lamaTraining;
+    }
+    public int getLama(){
+        return this.lamaTraining;
+    }
+    @Override
+    public void display(){
+        System.out.println("Nama : " + this.nama);
+        System.out.println("Umur : " + this.umur);
+        System.out.println("Lama Training : " this.lamaTraining);
+    }
+}
